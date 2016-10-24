f83d9b71bbffcc08272fcd6210f2d47965cce16c
Anastasios Zouzias <zouzias@gmail.com>
2016-10-24 22:50:13 +0200

    hotfix: scale_in_linkage

--- src/main/scala/org/zouzias/spark/lucenerdd/LuceneRDD.scala
+++ src/main/scala/org/zouzias/spark/lucenerdd/LuceneRDD.scala
@@ -150,16 +150,20 @@ class LuceneRDD[T: ClassTag](protected val partitionsRDD: RDD[AbstractLuceneRDDP
     : RDD[(T1, Array[SparkScoreDoc])] = {
     logInfo("Linkage requested")
     val monoid = new TopKMonoid[SparkScoreDoc](topK)(SparkScoreDoc.descending)
-    logDebug("Collecting query points to driver")
-    val queries = other.map(searchQueryGen).collect()
-    logDebug("Query points collected to driver successfully")
-    logDebug("Broadcasting query points")
+    logInfo("Collecting query points to driver")
+    val queriesString = other.map(searchQueryGen).collect().mkString("$").getBytes("UTF-8")
+    logInfo(s"Uncompressed queries: ${queriesString.length / 1024.0} MB")
+    val queries = Gzip.compress(queriesString)
+    logInfo(s"Compressed queries: ${queries.length / 1024.0} MB")
+    logInfo("Query points collected to driver successfully")
+    logInfo("Broadcasting query points")
     val queriesB = partitionsRDD.context.broadcast(queries)
-    logDebug("Query points broadcasting was successfully")
+    logInfo("Query points broadcasting was successfully")
 
     val resultsByPart: RDD[(Long, TopK[SparkScoreDoc])] = partitionsRDD.mapPartitions(partitions =>
         partitions.flatMap { case partition =>
-          queriesB.value.zipWithIndex.par.map { case (qr, index) =>
+          Gzip.decompress(queriesB.value).getOrElse("")
+            .split('$').zipWithIndex.par.map { case (qr, index) =>
             val results = partition.query(qr, topK)
               .map(x => monoid.build(x))
 
@@ -169,7 +173,7 @@ class LuceneRDD[T: ClassTag](protected val partitionsRDD: RDD[AbstractLuceneRDDP
         }
         , preservesPartitioning = true)
 
-    logDebug("Compute topK linkage per partition")
+    logInfo("Compute topK linkage per partition")
     val results = resultsByPart.reduceByKey(monoid.plus)
 
     other.zipWithIndex.map(_.swap).join(results).values
